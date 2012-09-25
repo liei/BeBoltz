4869b4a48b29f657404ec8661764a437f468ce8a
Expez <mrgrandmother@gmail.com>
2012-09-25 10:48:01 +0200

    Add bias layers to rbm constructor

--- src/edu/ntnu/beboltz/rbm/Rbm.java
+++ src/edu/ntnu/beboltz/rbm/Rbm.java
@@ -4,6 +4,8 @@ import java.util.Random;
 public class Rbm {
 	
 	private double[][] weights;
+	private double[] hiddenLayerBias;
+	private double[] visibleLayerBias;
 
 	/**
 	 * @param numVisible Number of visible nodes in RBM
@@ -11,6 +13,7 @@ public class Rbm {
 	 * Creates random weights between the nodes.
 	 */
 	public Rbm(int numVisible, int numHidden) {
+		assert(numVisible > 0 && numHidden > 0);
 		weights = new double[numVisible][numHidden];
 		Random random = new Random();
 		double high = 4 * Math.sqrt(6 / (numHidden + numVisible));
@@ -19,6 +22,17 @@ public class Rbm {
 			for (int j = 0; j < weights[i].length; j++) {
 				weights[i][j] = low + high * random.nextDouble();
 			}
+		}
+		
+		hiddenLayerBias = new double[numHidden];
+		visibleLayerBias = new double[numVisible];
+
+		for (int weight = 0; weight < hiddenLayerBias.length; weight++) {
+			hiddenLayerBias[weight] = 0;
+		}
+		
+		for (int weight = 0; weight < visibleLayerBias.length; weight++) {
+			hiddenLayerBias[weight] = 0;
 		}
 	}
 		
