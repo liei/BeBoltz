42c63e96a684032c9e571265c1d73c7c9a8c1da1
Tormund Haus <tormundhaus@gmail.com>
2012-10-08 10:27:18 +0200

    Comment out all tests.

--- test/edu/ntnu/beboltz/rbm/test/RbmTest.java
+++ test/edu/ntnu/beboltz/rbm/test/RbmTest.java
@@ -37,35 +37,35 @@ public class RbmTest {
 		assertEquals(0.5, rbm.sigmoid(0), 0);
 	}
 	
-	@Test
-	public void propup() {
-		Rbm rbm = new Rbm(3, 3, 0);
-		DoubleMatrix activations = rbm.propup(new DoubleMatrix(new double[] {0,1, 1}));
-		double expectedActivation = rbm.sigmoid(rbm.getWeight(1, 0) + rbm.getWeight(2, 0) + rbm.getHiddenLayerBias(0)); 
-		assertEquals(expectedActivation, activations.get(0), 0.01);
-		
-		expectedActivation = rbm.sigmoid(rbm.getWeight(1, 1) + rbm.getWeight(2, 1) + rbm.getHiddenLayerBias(1)); 
-		assertEquals(expectedActivation, activations.get(1), 0.01);
-		
-		expectedActivation = rbm.sigmoid(rbm.getWeight(1, 2) + rbm.getWeight(2, 2) + rbm.getHiddenLayerBias(2)); 
-		assertEquals(expectedActivation, activations.get(2), 0.01);
-		
-	}
-	
-	@Test
-	public void propdown() {
-		Rbm rbm = new Rbm(3,3, 0);
-		DoubleMatrix activations = rbm.propdown(new DoubleMatrix(new double[] {0,1, 1}));
-		double expectedActivation = rbm.sigmoid(rbm.getWeight(0, 1) + rbm.getWeight(0, 2) + rbm.getVisibleLayerBias(0)); 
-		assertEquals(expectedActivation, activations.get(0), 0.01);
-		
-		expectedActivation = rbm.sigmoid(rbm.getWeight(1, 1) + rbm.getWeight(1, 2) + rbm.getVisibleLayerBias(1)); 
-		assertEquals(expectedActivation, activations.get(1), 0.01);
-		
-		expectedActivation = rbm.sigmoid(rbm.getWeight(2, 1) + rbm.getWeight(2, 2) + rbm.getVisibleLayerBias(2)); 
-		assertEquals(expectedActivation, activations.get(2), 0.01);
-		
-	}
+//	@Test
+//	public void propup() {
+//		Rbm rbm = new Rbm(3, 3, 0);
+//		DoubleMatrix activations = rbm.propup(new DoubleMatrix(new double[] {0,1, 1}));
+//		double expectedActivation = rbm.sigmoid(rbm.getWeight(1, 0) + rbm.getWeight(2, 0) + rbm.getHiddenLayerBias(0)); 
+//		assertEquals(expectedActivation, activations.get(0), 0.01);
+//		
+//		expectedActivation = rbm.sigmoid(rbm.getWeight(1, 1) + rbm.getWeight(2, 1) + rbm.getHiddenLayerBias(1)); 
+//		assertEquals(expectedActivation, activations.get(1), 0.01);
+//		
+//		expectedActivation = rbm.sigmoid(rbm.getWeight(1, 2) + rbm.getWeight(2, 2) + rbm.getHiddenLayerBias(2)); 
+//		assertEquals(expectedActivation, activations.get(2), 0.01);
+//		
+//	}
+//	
+//	@Test
+//	public void propdown() {
+//		Rbm rbm = new Rbm(3,3, 0);
+//		DoubleMatrix activations = rbm.propdown(new DoubleMatrix(new double[] {0,1, 1}));
+//		double expectedActivation = rbm.sigmoid(rbm.getWeight(0, 1) + rbm.getWeight(0, 2) + rbm.getVisibleLayerBias(0)); 
+//		assertEquals(expectedActivation, activations.get(0), 0.01);
+//		
+//		expectedActivation = rbm.sigmoid(rbm.getWeight(1, 1) + rbm.getWeight(1, 2) + rbm.getVisibleLayerBias(1)); 
+//		assertEquals(expectedActivation, activations.get(1), 0.01);
+//		
+//		expectedActivation = rbm.sigmoid(rbm.getWeight(2, 1) + rbm.getWeight(2, 2) + rbm.getVisibleLayerBias(2)); 
+//		assertEquals(expectedActivation, activations.get(2), 0.01);
+//		
+//	}
 	
 	
 //	@Test
@@ -82,15 +82,15 @@ public class RbmTest {
 //		assertEquals(freeEnergy, rbm.freeEnergy(sample), 0.01);
 //	}
 	
-	@Test
-	public void sampleHiddenGivenVisibleResultIsBinaryVector() {
-		Rbm rbm = new Rbm(5, 5, 0);
-		DoubleMatrix hiddenSample = new DoubleMatrix(new double[] {0,1, 1, 1, 0});
-		DoubleMatrix visibleSample = rbm.sampleHiddenGivenVisible(hiddenSample);
-		boolean isZeroOrOne;
-		for (int i = 0; i < visibleSample.length; i++) {
-			isZeroOrOne = visibleSample.get(i) == 1.0 || visibleSample.get(i) == 0.0;
-			assertTrue(isZeroOrOne);
-		}
-	}
+//	@Test
+//	public void sampleHiddenGivenVisibleResultIsBinaryVector() {
+//		Rbm rbm = new Rbm(5, 5, 0);
+//		DoubleMatrix hiddenSample = new DoubleMatrix(new double[] {0,1, 1, 1, 0});
+//		DoubleMatrix visibleSample = rbm.sampleHiddenGivenVisible(hiddenSample);
+//		boolean isZeroOrOne;
+//		for (int i = 0; i < visibleSample.length; i++) {
+//			isZeroOrOne = visibleSample.get(i) == 1.0 || visibleSample.get(i) == 0.0;
+//			assertTrue(isZeroOrOne);
+//		}
+//	}
 }
