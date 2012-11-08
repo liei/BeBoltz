5b0d4cfbb4abd2f4b85544afe8b8a2552cea0767
Tormund Haus <tormundhaus@gmail.com>
2012-11-08 12:50:09 +0100

    SoftmaxRegression extends Layer.

--- src/edu/ntnu/beboltz/logreg/SoftmaxRegression.java
+++ src/edu/ntnu/beboltz/logreg/SoftmaxRegression.java
@@ -2,11 +2,12 @@ package edu.ntnu.beboltz.logreg;
 
 import java.io.IOException;
 
+import edu.ntnu.beboltz.mlp.Layer;
 import edu.ntnu.beboltz.util.ArrayUtil;
 import edu.ntnu.beboltz.util.DataSet;
 import edu.ntnu.beboltz.util.Util;
 
-public class SoftmaxRegression {
+public class SoftmaxRegression extends Layer<double[]>{
 
 	private static final double DEFAULT_LEARNING_RATE = 0.13;
 
@@ -18,11 +19,11 @@ public class SoftmaxRegression {
 	
 	private double learningRate;
 	
-	public SoftmaxRegression(int numInputs, int numClasses){
-		this(numInputs,numClasses,DEFAULT_LEARNING_RATE);
+	public SoftmaxRegression(int numClasses, int numInputs){
+		this(numClasses,numInputs,DEFAULT_LEARNING_RATE);
 	}
 	
-	public SoftmaxRegression(int numInputs, int numClasses, double lr){
+	public SoftmaxRegression(int numClasses, int numInputs, double lr){
 		this.numInputs  = numInputs;
 		this.numClasses = numClasses;
 		
@@ -33,7 +34,7 @@ public class SoftmaxRegression {
 	}
 	
 	public int classify(double[] input){
-		double[] y = Util.softmax(weights, bias, input);
+		double[] y = activate(input);
 		int bestCategory = 0;
 		for(int i = 1; i < y.length; i++){
 			if(y[i] > y[bestCategory]){
@@ -48,36 +49,94 @@ public class SoftmaxRegression {
 		return y[category];
 	}
 	
-	public double loss(DataSet ds){
+	public double loss(DataSet<double[]> ds){
 		double sum = 0;
-		for(DataSet.Item item : ds){
-			sum += Math.log(probability(item.image,item.label));
+		for(DataSet.Item<double[]> item : ds){
+			sum += Math.log(probability(item.data,item.label));
 		}
 		return -sum;
 	}
 	
-	public void train(DataSet training, DataSet testing, int maxEpochs){
+	public void train(DataSet<double[]> trainingSet, DataSet<double[]> validationSet,double minError, int maxEpochs){
 		double start,stop;
-		double errorRate = 1.0;
-		for(int epoch = 0; errorRate > 0.075 && epoch < maxEpochs; epoch++){
+		double error = 1.0;
+		for(int epoch = 0; error > minError && epoch < maxEpochs; epoch++){
 			System.out.printf("\tepoch %d...",epoch);
 			start = System.currentTimeMillis();
-			for(DataSet batch : training.split(100)){
+			for(DataSet<double[]> batch : trainingSet.split(100)){
 				update(batch);
 			}
 			stop = System.currentTimeMillis();
-			errorRate = validate(testing);
-			System.out.printf("done %.2f s  (error: %.2f%%)%n",(stop-start)/1000,errorRate*100);
+			error = validate(validationSet);
+			System.out.printf("done %.2f s  (error: %.2f%%)%n",(stop-start)/1000,error*100);
+		}
+	}
+
+	public void train(DataSet<double[]> trainingSet, DataSet<double[]> validationSet,int maxEpochs){
+		int batchSize = 100;
+		int numBatches = trainingSet.size() / batchSize;
+		
+		int patience = 5000;		// look as this many examples regardless
+		int patienceIncrease = 2;	// wait this much longer when a new best is
+									// found
+		double improvementThreshold = 0.999;  // a relative improvement of this much is
+				                               // considered significant
+		double validationFrequency = Math.min(numBatches, patience / 2.0);
+				                                  // go through this many
+				                                  // minibatche before checking the network
+				                                  // on the validation set; in this case we
+				                                  // check every epoch
+		
+		double bestValidationLoss = Double.POSITIVE_INFINITY;
+		
+		boolean doneLooping = false;
+		for(int epoch = 0;(epoch < maxEpochs) && (!doneLooping); epoch++){
+			int batchIndex = 0;
+			System.out.printf("\tepoch %d...",epoch);
+			double start = System.currentTimeMillis();
+			for(DataSet<double[]> batch : trainingSet.split(batchSize)){
+				update(batch);
+				// iteration number
+				int iter = epoch * numBatches + batchIndex;
+
+
+				
+				if ((iter + 1) % validationFrequency == 0){
+					// compute zero-one loss on validation set
+					double validationLoss = validate(validationSet);
+
+				    System.out.printf("minibatch %d/%d, validation error %f %%%n",
+				    		batchIndex + 1, numBatches,validationLoss * 100);
+
+				    // if we got the best validation score until now
+				    if(validationLoss < bestValidationLoss){
+				    	//improve patience if loss improvement is good enough
+				    	if(validationLoss < bestValidationLoss * improvementThreshold){
+				    		patience = Math.max(patience, iter * patienceIncrease);
+				    	}
+				    	bestValidationLoss = validationLoss;
+
+				    	if(patience <= iter){
+				    		doneLooping = true;
+				    		break;
+				    	}
+				    }
+				}
+				batchIndex++;
+			}
+			double stop = System.currentTimeMillis();
+			System.out.printf("done %.2f s%n",(stop-start)/1000);
 		}
 	}
 	
-	public void update(DataSet ds){
+	public void update(DataSet<double[]> ds){
 		
 		double[][] weightsGrad = new double[weights.length][weights[0].length];
 		double[]   biasGrad   = new double[bias.length];
 		for(int j = 0; j < numClasses; j++){
-			for(DataSet.Item item : ds){
-				double[] x_i = item.image;
+			for(DataSet.Item<double[]> item : ds){
+				setInput(item.data);
+				double[] x_i = input;
 				double p = indicator(j == item.label) - probability(x_i,j);
 				for(int l = 0; l < x_i.length; l++){
 					weightsGrad[j][l] -= x_i[l] * p; 
@@ -98,14 +157,19 @@ public class SoftmaxRegression {
 		return b ? 1.0 : 0.0;
 	}
 	
-	private double validate(DataSet validation) {
+	private double validate(DataSet<double[]> validation) {
 		double wrong = 0;
-		for(DataSet.Item item : validation){
-			if(item.label != classify(item.image)){
+		for(DataSet.Item<double[]> item : validation){
+			if(item.label != classify(item.data)){
 				wrong++;
 			}
 		}
 		return wrong / validation.size();
+	}
+
+	@Override
+	protected double[] activate() {
+		return Util.softmax(weights, bias, input);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -113,20 +177,19 @@ public class SoftmaxRegression {
 		
 		System.out.printf("loading...");
 		start = System.currentTimeMillis();		
-		DataSet trainingSet = DataSet.loadWithLabels(DataSet.IMAGE_FILE,  DataSet.LABEL_FILE);
-		DataSet validationSet = DataSet.loadWithLabels(DataSet.TEST_IMAGES, DataSet.TEST_LABELS);
+		DataSet<double[]> trainingSet = DataSet.loadWithLabels(DataSet.TRAIN_IMAGES,  DataSet.TRAIN_LABELS);
+		DataSet<double[]> validationSet = DataSet.loadWithLabels(DataSet.TEST_IMAGES, DataSet.TEST_LABELS);
 		stop = System.currentTimeMillis();
 		System.out.printf("done (%.2f)%n",(stop-start)/1000);
 
 		
 		SoftmaxRegression lr = new SoftmaxRegression(
-				trainingSet.getImageHeight() * trainingSet.getImageWidth(),10);
+				10,trainingSet.getItem(0).data.length);
 
 		System.out.printf("training...%n");
 		start = System.currentTimeMillis();
 		lr.train(trainingSet, validationSet, 100);
 		stop = System.currentTimeMillis();
 		System.out.printf("done (%.2f)%n",(stop-start)/1000);
-		
 	}
 }
